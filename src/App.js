debe861cdd937d5515128a0f1bac5f287de57b8e
Liviu Preda <liviupreda@gmail.com>
2019-12-03 08:31:37 +0200

    add DogDetails

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import DogList from "./DogList";
+import DogDetails from "./DogDetails";
 import { Switch, Route } from "react-router-dom";
 import whiskey from "./images/whiskey.jpg";
 import hazel from "./images/hazel.jpg";
@@ -42,8 +43,22 @@ class App extends Component {
     ]
   };
   render() {
+    const getDog = props => {
+      let name = props.match.params.name;
+      let curDog = this.props.dogs.find(
+        dog => dog.name.toLowerCase() === name.toLowerCase()
+      );
+      return <DogDetails {...props} dog={curDog} />;
+    };
     return (
-      <Route path="/dogs" render={() => <DogList dogs={this.props.dogs} />} />
+      <Switch>
+        <Route
+          exact
+          path="/dogs"
+          render={() => <DogList dogs={this.props.dogs} />}
+        />
+        <Route exact path="/dogs/:name" render={getDog} />
+      </Switch>
     );
   }
 }
